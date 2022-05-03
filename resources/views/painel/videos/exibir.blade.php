35b0660b3bbb53120e77d2961cac85873466d1c0
Luis Gustavo <gusouza980@gmail.com>
2022-05-03 11:25:00 -0300

    Update

--- resources/views/painel/videos/exibir.blade.php
+++ resources/views/painel/videos/exibir.blade.php
@@ -10,7 +10,7 @@
     <div class="col-12">
         <div class="card">
             <div class="card-body">
-                <div class="row">
+                <div class="row align-items-end">
                     @foreach($videos as $video)
                         <div class="col-xl-6 mb-3">
                             <h4 class="card-title">{{$video->titulo}}</h4>

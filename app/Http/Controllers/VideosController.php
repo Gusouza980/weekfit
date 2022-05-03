7260fe0ae1d3924f25bdd7f59538c4dd35b4ddb4
Luis Gustavo <gusouza980@gmail.com>
2022-05-03 11:16:36 -0300

    Update

--- app/Http/Controllers/VideosController.php
+++ app/Http/Controllers/VideosController.php
@@ -29,8 +29,8 @@ class VideosController extends Controller
     }
 
     public function editar(Video $video){
-        phpinfo();
-        die();
+        // phpinfo();
+        // die();
         return view("painel.videos.edicao", ["video" => $video]);
     }
 
